cf5f7e7edf699cc458630877b3d2e0a38e0997a7
tono <yuta925@gmail.com>
2024-01-07 16:50:57 +0900

    js確認中

--- js/main.js
+++ js/main.js
@@ -1,75 +1,25 @@
-  
-  $(document).ready(function() {
-    $('.drawer').drawer();
-  });
-
-document.addEventListener('DOMContentLoaded', function () {
-
-    const btn = document.querySelector('.totop');
-    const header = document.querySelector('.header');
-
-    btn.addEventListener('click', (e) => {
-        e.preventDefault();
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
-    })
 
 
-    const newsTop = document.querySelector('.news');
-    // const body = document.querySelector('body');
-    const options = {
-        rootMargin: "400px 0px 0px 0px",
-      };
-
-
-    const observer = new IntersectionObserver(inViewToTop, options);
-    observer.observe(newsTop);
-
-
-    function inViewToTop(entries) {
-        // 交差検知をしたもののなかで、isIntersectingがtrueのDOMを色を変える関数に渡す
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            btn.classList.add('inview');
-            header.classList.add('inview');
-        }
+// window.addEventListener('load', function(){
+  $(function() {
+    $.fn.extend({
+      randomPh : function(num) {
+        return this.each(function() {
+          var chn = $(this).children().hide().length;
+          for(var i = 0; i < num && i < chn; i++) {
+            var r = parseInt(Math.random() * (chn - i)) + i;
+          $(this).children().eq(r).show().prependTo($(this));
+          }
         });
       }
-
-      
-      jQuery('.qa__title').on('click', function() {
-        jQuery(this).next().slideToggle();
     });
-
-
-    const options2 = {
-      cover: true,
-      heightRatio: 0.5,
-      perMove: 1,
-      gap: 16,
-      updateOnMove: true,
-      padding: "5rem", //左右のスライドを5rem分表示させる
-      // type: "loop", //スライドをループさせる
-      focus: "right", //アクティブなスライドを中央に表示する
-    };
-    
-    const splide = new Splide(".splide", options2);
-    splide.mount();
-
-    
+  
+    $("[randomPh]").each(function() {
+      $(this).randomPh($(this).attr("randomPh"));
+    });
+  
   });
+// });
 
 
 
-  $(function() {
-    $('.header__li').on('click', function() {
-      $(".pulldown").slideToggle();
-  });
-
-    $('.top__title').on('click', function() {
-      
-      $('.top__sub').toggle("うんち");
-  });
-  });
